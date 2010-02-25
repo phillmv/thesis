e575adcad01df51038a492714c8998dfc9fa58b5
Phill MV <phill.mv@gmail.com>
2010-02-24 21:14:20 -0500

    Added some endless pageless/infinite scroll. Added classifiations to the UI. Performed some changes to the subscription view, enabling us to see all entries against a certain url.

--- app/controllers/magazine_controller.rb
+++ app/controllers/magazine_controller.rb
@@ -1,6 +1,25 @@
 class MagazineController < ApplicationController
 
   def index
-    @entries = Entry.find(:all, :order => "published DESC", :limit => 60)
+    @entries = Entry.paginate(:page => params[:page],
+                              :per_page => 5,
+                              :order => "published DESC",
+                              :conditions => { :read => nil })
+  end
+
+  def more
+    @entries = Entry.paginate(:page => params[:page],
+                              :per_page => 10,
+                              :order => "published DESC",
+                              :conditions => { :read => nil })
+
+    render :index, :layout => false
+  end
+
+  def read
+    @entry = Entry.find(params[:id])
+    @entry.read!
+
+    render :text => "OK"
   end
 end
