bed6c96f035224891014552d1d9a99f5bf8deec8
Phill MV <phill.mv@gmail.com>
2010-03-04 19:00:58 -0500

    First steps in feeding text to the classifier, alongside with some feed massaging.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,3 +1,11 @@
 class Classification < ActiveRecord::Base
   belongs_to :entry
+
+  def self.liked
+    Entry.find_by_sql("select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is true);")
+  end
+
+  def self.disliked
+    Entry.find_by_sql("select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is false);")
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,13 @@
 class Entry < ActiveRecord::Base
+  # taken from: 
+  # http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/
+  VALID_URL = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
+  # Internal field separator
+  # If this ever changes the classifier must be retrained
+  # classifier strips out all common punctuation, so uncommon char = win
+  IFS = '§'
+    
   belongs_to :subscription
   has_one :classification
 
@@ -13,13 +22,33 @@ class Entry < ActiveRecord::Base
       e = Entry.find_or_create_by_url(entry.url)
       e.title = entry.title
       e.subscription = subscription
-      e.summary = entry.summary
-      e.content = entry.content
+      
+      e.content = massage_html(e, entry.content) unless entry.content == nil
+      e.summary = massage_html(e, entry.summary) unless entry.summary == nil
+
       e.published = entry.published
       e.author = entry.author
 
       e.save!
     }
+  end
+
+  def classifier_text
+    # let's start simple. Ideally should be adding qualifiers like
+    # weighing link text more heavily.
+
+    prefix("author", self.author) << 
+      prefix("subscription", self.subscription.title) <<
+        prefix("title", self.title) <<
+          Hpricot(self.essence).to_plain_text.strip
+  end
+  
+  def essence
+    if content.nil?
+      return summary
+    else
+      return content
+    end
   end
 
   def read!
@@ -67,6 +96,46 @@ class Entry < ActiveRecord::Base
     end
     self.classification[attribute] = val
     self.classification.save!
+  end
+
+  # appends prefixes to fields
+  # assumes field is a single word, removes spaces from text and appends a 
+  # space at the end for serial concatenation.
+  def prefix(field = nil, text = nil)
+    return " " if text.nil?
+    "#{field}#{IFS}#{text.gsub(/ /,'')} "
+  end
+
+  def self.massage_html(entry, text)
+    h = Hpricot(text)
+
+    # Can you believe some people use script js includes instead of
+    # object tags for videos? This broke the app 'cos a faulty script 
+    # replaced the whole DOM with the object tag instead of just magically 
+    # figuring out where it was supposed to go
+    # What a braindead pattern; pretty sure Google Reader does this too.
+
+    h.search("script").remove # <3 Hpricot
+
+    # fuck you, foreign policy blog
+    # (in their cms they never add the domain for their blog pics)
+    # I am positive google reader does this exact same thing
+
+    # TODO: Validate subscription URLs such that they always have a 
+    # leading forward slash (/)
+    h.search("img").each do |elem| 
+      if !(VALID_URL === elem.attributes['src']) then
+        new_src = (entry.subscription.url + elem.attributes['src'])
+
+        # Let's be sure I don't fuck this up.
+        if VALID_URL === new_src then
+          elem.attributes['src'] = new_src
+        end
+      end
+    end
+    
+    h.inner_html
+
   end
 
 end
