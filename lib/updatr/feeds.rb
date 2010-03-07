3c5b23a50ab1d5b35e02b13cc7652f6ef9e82601
Phill MV <phill.mv@gmail.com>
2010-03-07 15:16:14 -0500

    Refactored daemon to allow other services to be background processed. Needs further refactoring.

---
+++ lib/updatr/feeds.rb
@@ -0,0 +1,123 @@
+require 'feedzirra'
+
+# basically a singleton or a namespace; wrapping these methods as class
+# methods feels 'wrong', but I haven't the time to lookup a better pattern
+# expects ActiveRecord to be loaded; I wonder again
+
+#TODO:  Some kind of refactoring? I like the idea of there being an abstract
+# class that all services can inherit from. I also have the sneaking
+# suspicion that there is almost certainly a worker framework I could've
+# used instead.
+
+class FeedUpdater
+
+  def initialize(sleep_period)
+    @feeds = {}
+    @sleep_for = sleep_period
+    @last_update = Time.now
+    init()
+  end
+
+  def update!
+    if (@last_update + @sleep_for) <= Time.now then
+      puts "Updating. It's currently: #{Time.now}" if DEBUG
+
+      @last_update = Time.now
+      # TODO: does not remove deleted items off update list until daemon 
+      # restart
+      if @sub_count != Subscription.size || @last_sub != Subscription.last then
+        log("Feed list has changed. Refetching feeds.")
+        init() #reinitalizes the instance variables, fetches new updates.
+
+      else
+        count = 0
+        @subscriptions.each do |sub|
+          if load_feeds!(sub.url) then
+            count = count + 1
+
+          end
+
+        end
+
+        log "FeedUpdater: updated #{count} feeds."
+
+      end
+      return true
+
+    else
+      puts "Not time yet. It's currently: #{Time.now}" if DEBUG
+      return false
+
+    end
+  end
+
+  private
+  def load_feeds!(url)
+    begin
+      feed = Feedzirra::Feed.update(@feeds[url])
+      if feed == 0 then
+        raise "Feed update failed for #{url}. How's your internet connection?"
+      else
+        @feeds[url] = feed
+      end
+
+      if feed.updated? then
+        log "#{url} has updates." if DEBUG
+        Subscription.find_by_url(url).add_entries(feed.new_entries)
+        return true
+
+      end
+
+    rescue Exception => e
+      log e.inspect
+      log e.backtrace
+    end
+
+    return false
+  end
+
+  def init
+    @subscriptions = Subscription.all
+    @sub_count = Subscription.size  
+    @last_sub = @subscriptions.last
+    log("Sub count: #{@sub_count}")
+
+    count = 0
+
+    @subscriptions.each do |sub|
+      log sub.url if DEBUG
+
+      begin  
+        if @feeds[sub.url] != nil then
+          if load_feeds!(sub.url) then
+            count = count + 1
+
+          end
+
+        else
+          feed = Feedzirra::Feed.fetch_and_parse(sub.feed_url)
+          if feed == 0 then
+            # change sub count to force reinit on next cycle.
+            @sub_count = @sub_count - 1
+            raise "Feed init for #{sub.url} failed. How's your internet connection?"
+          else
+            @feeds[sub.url] = feed
+          end
+          Subscription.find_by_url(sub.url).
+            add_entries(@feeds[sub.url].entries)     
+          count = count + 1
+
+        end
+
+      rescue Exception => e
+        log e.inspect
+        log e.backtrace
+
+      end
+
+    end
+
+    log "Feeds with updates: #{count}"
+  end
+
+end
--- lib/updatr/populate.rb
+++ lib/updatr/populate.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 require File.expand_path('../../../config/environment',  __FILE__)
 require 'feedzirra'
+require File.dirname(__FILE__) + '/feeds'
 
-#cmdline option, 'debug' will log more & will not sleep.
+# cmdline option, 'debug' will log more & will not sleep.
+# This needs to be pushed out to configuration.
 DEBUG = (ARGV[0] == "debug") ? true : false
-SLEEPTIME = DEBUG ? 3 : 1800 # 30mins
+SLEEPTIME = DEBUG ? 30.seconds.to_i : 3.minutes.to_i
+
+FEED_SLEEP = 30.minutes
 
 # You know, I have a feeling I didn't have to write this.
 def log(msg, feeds = nil)
@@ -14,106 +18,25 @@ def log(msg, feeds = nil)
 
 end
 
-# initialize feeds.
-@feeds = {}
-def init
-  @subscriptions = Subscription.all
-  @sub_count = Subscription.size  
-  @last_sub = @subscriptions.last
-  log("Sub count: #{@sub_count}")
-
-  count = 0
-
-  @subscriptions.each do |sub|
-    log sub.url if DEBUG
-
-    begin  
-      if @feeds[sub.url] != nil then
-        if update!(sub.url) then
-          count = count + 1
-        
-        end
-
-      else
-        feed = Feedzirra::Feed.fetch_and_parse(sub.feed_url)
-        if feed == 0 then
-          # change sub count to force reinit on next cycle.
-          @sub_count = @sub_count - 1
-          raise "Feed init for #{sub.url} failed. How's your internet connection?"
-        else
-          @feeds[sub.url] = feed
-        end
-        Subscription.find_by_url(sub.url).
-          add_entries(@feeds[sub.url].entries)     
-        count = count + 1
-
-      end
-
-    rescue Exception => e
-      log e.inspect
-      log e.backtrace
-
-    end
-
-  end
-
-  log "Feeds with updates: #{count}"
-end
-
-def update!(url)
-  begin
-    feed = Feedzirra::Feed.update(@feeds[url])
-    if feed == 0 then
-      raise "Feed update failed for #{url}. How's your internet connection?"
-    else
-      @feeds[url] = feed
-    end
-
-    if feed.updated? then
-      log "#{url} has updates." if DEBUG
-      Subscription.find_by_url(url).add_entries(feed.new_entries)
-      return true
-
-    end
-
-  rescue Exception => e
-    log e.inspect
-    log e.backtrace
-  end
-
-  return false
-end
-
-
 log("### Starting up... ###\n\n")
-
-init()
+@services = []
+@services << FeedUpdater.new(FEED_SLEEP)
 # When we initialized our variables, we processed all the new entries.
 # Let's wait a standard period before pinging all of feeds again.
+
 sleep SLEEPTIME
 
 loop do
   
-  log("Awake.")
-
-  # TODO: does not remove deleted items off update list until daemon restart
-  if @sub_count != Subscription.size || @last_sub != Subscription.last then
-    log("Feed list has changed. Refetching feeds.")
-    init() #reinitalizes the instance variables, fetches new updates.
-
-  else
-    count = 0
-    @subscriptions.each do |sub|
-        if update!(sub.url) then
-          count = count + 1
-        
-        end
-
-    end
-
-    log "Feeds with updates: #{count}"
-
+  # This feels unsatisfactory. The idea behind this is that I now have a
+  # need for multiple background processing. I kind of want this to be
+  # asynchronous - what if a service hangs? Right now processing takes 
+  # about ten seconds, so it's not a big deal.
+  
+  @services.each do |serv|
+    serv.update!
+  
   end
-
+  
   sleep SLEEPTIME 
 end
