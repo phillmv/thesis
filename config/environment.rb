1625179c9104b68c92c3895b568b6b6de8e9150a
Phill MV <phill.mv@gmail.com>
2010-06-03 02:21:44 -0400

    Changed to EST.

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 require File.join(File.dirname(__FILE__), '../lib/extensions.rb')
 
 Rails::Initializer.run do |config|
+
+  config.time_zone = "Eastern Time (US & Canada)"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
