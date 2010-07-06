4b318a6b3348e14962f40f8e2731993899bb5bca
Phill MV <phill.mv@gmail.com>
2010-07-06 15:43:42 -0400

    Mild convenient method for Classification.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,11 +2,11 @@ class Classification < ActiveRecord::Base
   belongs_to :entry
   belongs_to :user
 
-  def self.liked
-    Entry.find_by_sql("select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is true);")
+  def self.liked(user)
+    Entry.find_by_sql(["select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is true and classifications.user_id = ?);", user.id])
   end
 
-  def self.disliked
-    Entry.find_by_sql("select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is false);")
+  def self.disliked(user)
+    Entry.find_by_sql(["select * from entries where (select entry_id from classifications where entry_id = entries.id and classifications.liked is false and classifications.user_id = ?);", user.id])
   end
 end
