abe81c00ebd07cfd175bbc68c2db708668e89f1a
Phill MV <phill.mv@gmail.com>
2010-07-26 23:46:38 -0400

    Whodda thought that pg was on to something?

--- lib/errar/naive_bayes.rb
+++ lib/errar/naive_bayes.rb
@@ -73,9 +73,26 @@ class NaiveBayes
   # this is just the cumulative multiplication of all the word probabilities for this category
   def doc_probability(category, document)
     doc_prob = 1
+    word_probs = []
+    word_count(document).each { |word| 
+      #doc_prob *= word_probability(category, word[0]) 
+      word_probs << word_probability(category, word[0])
+    }
+
+    # paul graham's suggestion: fifteen most significant
+    word_probs.sort.reverse[0..15].each do |p|
+      doc_prob *= p
+    end
+    return doc_prob
+  end
+
+=begin UNCOMMENTED OUT ORIGINAL CODE
+  def doc_probability(category, document)
+    doc_prob = 1
     word_count(document).each { |word| doc_prob *= word_probability(category, word[0]) }
     return doc_prob
   end
+=end
 
   # the probability of a category
   # this is the probability that any random document being in this category
@@ -93,9 +110,9 @@ class NaiveBayes
     words = document.gsub(/[^\w\s]/,"").split
     d = Hash.new
     words.each do |word|
-      word.downcase! 
+      #word.downcase! 
       key = word.stem
-      unless COMMON_WORDS.include?(word) # remove common words
+      unless COMMON_WORDS.include?(word.downcase) # remove common words
         d[key] ||= 0
         d[key] += 1
       end
